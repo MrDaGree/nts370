6e9a8c932afea73e88b10d65a2b075ac5be76d5d
MrDaGree <dawsonmax10@gmail.com>
2020-01-29 13:48:22 -0700

    added project 1.4 files

---
+++ project1.4.md
@@ -0,0 +1,3 @@
+###Project 1.4
+
+https://github.com/MrDaGree/nts370/blob/master/project1.4.sh
---
+++ project1.4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for ((i=1;i<=100;i++))
+do
+    if [[ $(($i % 3)) == 0 && $(($i % 5)) == 0 ]];
+    then
+        echo "$i FizzBuzz"
+    elif [[ $(($i % 3)) == 0 ]];
+    then
+        echo "$i Fizz"
+    elif [[ $(($i % 5)) == 0 ]];
+    then
+        echo "$i Buzz"
+    else
+        echo "$i"
+    fi
+done
