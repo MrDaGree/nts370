c1715887ba310b1c18510bc837f3bb3854cd27eb
MrDaGree <dawsonmax10@gmail.com>
2020-02-27 14:33:16 -0700

    added comments to output file stuff

--- project2.3/project2.3.pl
+++ project2.3/project2.3.pl
@@ -17,20 +17,24 @@ if (-e $inputfilename && -r $inputfilename) {
 open(inputfile, "$inputfilename");
 
 
-
+#Ask for user input of what file in tmp the script will dump content to.
 print "Please enter the output file name: ";
 $outputfilename = <>;
-$outputfilename = "/tmp/" . $outputfilename;
+$outputfilename = "/tmp/" . $outputfilename; #Add '/tmp/' before hand so user just has to add filename
 chomp $outputfilename;
 
+#check if the output file exists already and if we can write to it.
 if (-e $outputfilename && -w $outputfilename) {
     print "'$outputfilename' is writeable, continuing..\n";
 } else {
     print "'$outputfilename' does not have write permissions or perhaps does not exist.\n";
 }
 
+#open the output file in writing mode
 open(outputfile, ">$outputfilename");
 
+#loop through each line of the input file, use regex to find '/home' and replace with '/export/home'
+#then output to the file.
 while (<inputfile>) {
     s/\/home/\/export\/home/;
     print outputfile $_;
